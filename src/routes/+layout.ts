86c0ef477ec7f0ae122c3b2bebfad12db3c54fbc
Arumi <arumi@thwiki.cc>
2023-05-05 05:36:34 +0800

    Disabled ssr

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,9 +3,13 @@ import type { LayoutLoad } from './$types';
 
 export const load = (async ({ url }) => {
 	const { pathname } = url;
-	const defaultLocale = 'zh';
+	const defaultLocale = window.navigator.language.startsWith('zh') ? 'zh' : 'en';
 	const initLocale = locale.get() || defaultLocale;
 	await loadTranslations(initLocale, pathname);
 
 	return {};
 }) satisfies LayoutLoad;
+
+export const ssr = false;
+
+export const prerender = true;
